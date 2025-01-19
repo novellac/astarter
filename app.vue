708e86648e297edd6d37552ba8d0d479aed432bf
novellac <38117965+novellac@users.noreply.github.com>
2025-01-19 13:43:38 -0500

    chore: Add Nuxt Image module

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+
+    <h1>Welcome to Aster, a starter repo</h1>
+    <NuxtImg src="/Aster_Tataricus.jpeg" alt="" />
+    <p>Image by <a href="https://commons.wikimedia.org/wiki/User:Pascalou_petit">Pascalou petit</a>, courtesy of <a href="https://commons.wikimedia.org/wiki/File:Aster_Tataricus.JPG">Wikimedia</a>.</p>
   </div>
 </template>
